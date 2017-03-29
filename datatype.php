e5485e0083aa72167f170596ca09b576e1019d68
sandeeplamp <sandeep.lamp@gmail.com>
2017-03-28 19:49:40 -0500

    Add files via upload

---
+++ datatype.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<body>
+<?php
+$x="Hello ";
+$y="world";
+echo $x;
+echo $y;
+?>
+</body>
+</html>
---
+++ var_dump.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$x = 5985;
+$y = 10.345;
+$cars = array("Volvo","BMW","Hyundai");
+var_dump($x);
+var_dump($y);
+var_dump($cars);
+?>  
+
+</body>
+</html>
